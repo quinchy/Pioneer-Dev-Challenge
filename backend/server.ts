50f7f99fb951ea64e7b5f6d92833aeb2e0398ac4
Cyril James De Guzman <deguzmancyriljames.dev@gmail.com>
2026-03-13 16:43:54 +0800

    feat: create dev server entry point and update package.json script to use dev.ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,4 @@
 // src/server.ts
 import app from "./app";
-import { env } from "./validation/env";
 
-const PORT = env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+export default app;
